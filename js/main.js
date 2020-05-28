6b3ad6453b3ed37e6ab5ca05fe5a09864e612058
MaxKopyl <m.s.kopyl.kharkov@gmail.com>
2020-05-28 19:13:25 +0300

    started recreate site, made header and started wrapper

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+})
+
+$(".first-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    items: 1,
+    navText: "",
+    autoplay: false,
+    
+})
