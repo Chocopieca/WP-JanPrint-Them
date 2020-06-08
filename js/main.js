f5a0fb85112b7b7cb0dd29d6cf154fa6943b3c12
MaxKopyl <m.s.kopyl.kharkov@gmail.com>
2020-06-08 16:03:24 +0300

    completed mobile's page cotalog

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ $(".first-carousel").owlCarousel({
     margin: 0,
     items: 1,
     navText: "",
-    autoplay: false,
 })
 
 $(".second-carousel").owlCarousel({
@@ -25,13 +24,46 @@ $(".third-carousel").owlCarousel({
     margin: 10,
     items: 1,
     navText: "",
-    autoplay: false,
     nav: false,
     dots: true,
     responsive: {
         768:{
             dots: false,
             nav: true
+        }
+    }
+})
+
+$('.catalog-carousel').owlCarousel({
+    loop:false,
+    margin:5,
+    nav:true,
+    navText: "",
+    items: 1,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:3
+        }
+    }
+})
+
+$('.inst-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:false,
+    navText: "",
+    items: 1,
+    dots: true,
+    autoplay: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:3
         }
     }
 })
