d05f736881e61a813c930d30bae5a0f290428a2b
MaxKopyl <m.s.kopyl.kharkov@gmail.com>
2020-06-15 13:42:33 +0300

    comleted contact page

--- js/main.js
+++ js/main.js
@@ -89,13 +89,13 @@ function taggleNavbar() {
 burger.onclick = taggleNavbar;
 
 // map
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
-  });
-}
+// var map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8
+//   });
+// }
 
     // if (openBurger.style.display === "none") {
 
